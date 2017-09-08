803c07509da4165b23c5b7d122370e87162d8a35
NessCollege <nesscollege2017@gmail.com>
2017-09-08 13:11:16 +0300

    Tada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,22 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  //declare some class fields.
+  name1 = 'Osher Ad'
+  name2 = 'Moana'
+  get score(){
+    var concat = (this.name1 + this.name2).toLowerCase();
+    var sum = 0;
+    // Arrow functions:
+    concat.split('').forEach(letter => {
+      sum += letter.charCodeAt(0);
+    })
+    return sum % 101;
   }
+  // Injection: 
+  constructor(public navCtrl: NavController) {}
 
 }
